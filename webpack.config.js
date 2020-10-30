b750984a028302dee83185a139c49fb4e7cd81ec
Robert Mruczek <rtmruczek@gmail.com>
2020-10-29 21:01:32 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/main.ts',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  devtool: 'inline-source-map',
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  mode: 'development',
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    writeToDisk: true,
+    open: true,
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'index.html',
+        },
+      ],
+    }),
+    new webpack.DefinePlugin({
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
+  ],
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
